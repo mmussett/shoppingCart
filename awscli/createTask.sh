62f5eec4f8e6b8070aa5e0bf6597ce49ccf2e23f
mmussett <mmussett@tibco.com>
2018-10-12 16:18:42 +0100

    Fixed if

--- awscli/createTask.sh
+++ awscli/createTask.sh
@@ -2,8 +2,9 @@
 
 TASK_DEFINITION_ARN=`aws ecs list-task-definitions --family-prefix ShoppingCart | jq '.taskDefinitionArns[0]'`
 
-if [ -z "$TASK_DEFINITION_ARN" ]
+if [ -n "$TASK_DEFINITION_ARN" ]
 then 
+
   aws ecs register-task-definition --cli-input-json file://./awscli/createTask.json
 fi
 
