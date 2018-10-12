7eaf2accbc37f9bb2c1c2d55a4d18e0ce0acf585
mmussett <mmussett@tibco.com>
2018-10-12 16:50:40 +0100

    Set desired count to 1

--- awscli/createService.sh
+++ awscli/createService.sh
@@ -6,8 +6,8 @@ TASK_REV=`aws ecs describe-task-definition --task-definition ShoppingCart | jq '
 if [ -n "$TASK_REV" ]
 then
 
-	aws ecs create-service --cluster bwce-cluster --service-name ShoppingCart --task-definition ShoppingCart:$TASK_REV --desired-count 2 --launch-type "FARGATE" --deployment-configuration "maximumPercent=200,minimumHealthyPercent=50"  --network-configuration "awsvpcConfiguration={subnets=[subnet-a55894ed],securityGroups=[sg-9f44cae5],assignPublicIp=ENABLED}"
-	aws application-autoscaling register-scalable-target --resource-id service/bwce-cluster/ShoppingCart --service-namespace ecs --scalable-dimension ecs:service:DesiredCount --min-capacity 2 --max-capacity 20 --role-arn arn:aws:iam::048063763713:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling
+	aws ecs create-service --cluster bwce-cluster --service-name ShoppingCart --task-definition ShoppingCart:$TASK_REV --desired-count 1 --launch-type "FARGATE" --deployment-configuration "maximumPercent=200,minimumHealthyPercent=50"  --network-configuration "awsvpcConfiguration={subnets=[subnet-a55894ed],securityGroups=[sg-9f44cae5],assignPublicIp=ENABLED}"
+	aws application-autoscaling register-scalable-target --resource-id service/bwce-cluster/ShoppingCart --service-namespace ecs --scalable-dimension ecs:service:DesiredCount --min-capacity 1 --max-capacity 20 --role-arn arn:aws:iam::048063763713:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling
 	aws application-autoscaling put-scaling-policy --cli-input-json file://./awscli/put-scaling-policy-customer.json
 else
 	exit -1
