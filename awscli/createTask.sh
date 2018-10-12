be4979fc0d220dcea76d142381823365bf182b1b
mmussett <mmussett@tibco.com>
2018-10-12 16:02:36 +0100

    Add Fargate Task Definition stage

--- awscli/createTask.sh
+++ awscli/createTask.sh
@@ -1,3 +1,8 @@
+#!/bin/sh
 
-aws ecs register-task-definition --cli-input-json file://./awscli/createTask.json
+TASK_DEFINITION_ARN=`aws ecs list-task-definitions --family-prefix ShoppingCart | jq '.taskDefinitionArns[0]'`
+if [ -z $TASK_DEFINITION_ARN ]
+then 
+  aws ecs register-task-definition --cli-input-json file://./awscli/createTask.json
+fi
 
