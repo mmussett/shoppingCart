a48319be1d9aeadf53c9629cd727e46691817fc3
mmussett <mmussett@tibco.com>
2018-10-12 15:32:59 +0100

    AWS Fargate cluster creation script

---
+++ awscli/createCluster.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+CLUSTER_ARN=`aws ecs describe-clusters --cluster bwce-cluster | jq '.clusters[0].clusterArn'
+
+if [ ! -z $CLUSTER_ARN ]
+then 
+aws ecs create-cluster --cluster-name bwce-cluster
+fi
+
+
+
