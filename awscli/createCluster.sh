31f1809d33530e18c0c240d114089991126dfe64
mmussett <mmussett@tibco.com>
2018-10-12 16:10:13 +0100

    Fixed check

--- awscli/createCluster.sh
+++ awscli/createCluster.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 CLUSTER_ARN=`aws ecs describe-clusters --cluster bwce-cluster | jq '.clusters[0].clusterArn'`
-echo $CLUSTER_ARN
 
-if [ -z $CLUSTER_ARN ]
+if [ -n "$CLUSTER_ARN" ]
 then 
 aws ecs create-cluster --cluster-name bwce-cluster
 fi
