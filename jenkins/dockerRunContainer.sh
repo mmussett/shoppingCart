381ddaf416180fb81fe3bb04fa03f3d950921d8b
Mark Mussett <mmussett@tibco.com>
2018-10-15 11:55:08 +0100

    Shopping Cart docker container link with BWCE Monitor

--- jenkins/dockerRunContainer.sh
+++ jenkins/dockerRunContainer.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+
 PUBLIC_HOSTNAME=`curl http://169.254.169.254/latest/meta-data/public-hostname`
-docker run -h $PUBLIC_HOSTNAME -d --name shoppingcart -p 18080:8080 -e BW_PROFILE=default  shoppingcart:latest
+docker run -h $PUBLIC_HOSTNAME -d --name shoppingcart -p 18081:8080 --link=bwcemon -e BW_APP_MONITORING_CONFIG='{"url":"http://172.31.46.1:18080"}' shoppingcart:latest
+
 
